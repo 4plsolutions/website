f3e988beb9bcc35bb94247772c2e924dbafc303a
Angelika Yoder <angelika.yoder@gmail.com>
2019-11-21 20:54:44 -0500

    Style area and footer

--- js/js.js
+++ js/js.js
@@ -1,20 +1,23 @@
-window.onscroll = function() {shrinkUp()};
-
-function shrinkUp() {
-  let mainLogo = document.getElementById("main-logo");
-  let navImg = document.getElementById("nav-img");
-  let navbar = document.getElementById("navbar");
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    mainLogo.className = "main-logo invisible";
-    navImg.className = "nav-img visible";
-    navImg.style.display = "flex";
-    navbar.className = "navbar invisible";
-  } else {
-    mainLogo.className = "main-logo visible ";
-    navImg.className = "nav-img invisible";
-    navImg.style.display = "none";
-    navbar.className = "navbar visible";
-
-
-  }
-}
+// window.onscroll = function() {shrinkUp()};
+//
+// function shrinkUp() {
+//   let mainLogo = document.getElementById("main-logo");
+//   let navImg = document.getElementById("nav-img");
+//   let navbar = document.getElementById("navbar");
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     mainLogo.className = "main-logo invisible";
+//     navImg.className = "nav-img visible";
+//     navImg.style.display = "flex";
+//     navbar.className = "navbar invisible";
+//   } else {
+//     mainLogo.className = "main-logo visible ";
+//     navImg.className = "nav-img invisible";
+//     navImg.style.display = "none";
+//     navbar.className = "navbar visible";
+//
+//
+//   }
+// }
+// function currentYear() {
+//   document.write(new Date().getFullYear());
+// }
